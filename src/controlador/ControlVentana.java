37cca94bd8b648b6f778bcc9941b8804423e7ad8
Manuel David Castano Saldarriaga <1005966201@academico.icesi.edu.co>
2019-10-26 10:46:27 -0500

    first commit

---
+++ src/controlador/ControlVentana.java
@@ -0,0 +1,19 @@
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import modelo.Buscaminas;
+
+public class ControlVentana implements Initializable{
+	
+	private Buscaminas buscaminas;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
