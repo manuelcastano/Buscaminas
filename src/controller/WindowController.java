34962815ed8206e9d76a179f95205695665b6d35
manuelcastano <you@example.com>
2019-11-04 14:49:32 -0500

    finished

--- src/controller/WindowController.java
+++ src/controller/WindowController.java
@@ -18,6 +18,7 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
